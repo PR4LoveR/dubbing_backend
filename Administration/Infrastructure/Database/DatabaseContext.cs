f1d9f82018a5c8b9f95cc127190b309044f95413
Roman Plyta <44133157+PR4LoveR@users.noreply.github.com>
2020-05-03 17:56:50 +0300

    Update DatabaseContext.cs

--- Administration/Infrastructure/Database/DatabaseContext.cs
+++ Administration/Infrastructure/Database/DatabaseContext.cs
@@ -20,7 +20,7 @@ namespace SoftServe.ITAcademy.BackendDubbingProject.Administration.Infrastructur
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localhost)\\MSSQLSERVER;Database = dubbingDB;Integrated Security=true;");
+            optionsBuilder.UseSqlServer("Server=newdatabase.c3dau9pnded7.eu-central-1.rds.amazonaws.com\\MSSQLSERVER, 1433;Database=myDataBase;User Id=root;Password=needforspeed;");
         }
     }
 }
